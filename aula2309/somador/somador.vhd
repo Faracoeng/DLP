c6b8ea83711258c3d6bc54672b0d599ad720e093
Faracoeng <alfmazucheli@gmail.com>
2019-09-23 08:50:05 -0300

    Exercicio somador compilando add comentários importantes inseridos

--- aula2309/somador/somador.vhd
+++ aula2309/somador/somador.vhd
@@ -20,11 +20,13 @@ architecture andrev1 of somador is
 	-- Declarations (optional)
 	signal a_uns : unsigned(N downto 0);
  	signal b_uns : unsigned(N downto 0);
-	--signal x_uns : unsigned(N downto 0); -- Nao precisa, da p mandar para a saida direto
+	--signal x_uns : unsigned(N downto 0); -- Nao precisa, da pra mandar para a saida direto
 
 begin
 	a_uns <= resize(unsigned(a), N+1);
 	b_uns <= resize(unsigned(b), N+1);
 	x <= std_logic_vector(a_uns + b_uns); -- enviando a soma direto para a saida
-
+	-- OU da para fazer de outra forma
+	-- x_uns <= (a_uns + b_uns)
+	-- x = std_logic_vector(x)
 end architecture;
