e4db17cbeb2cb4bb5d761189c2cf5fb36be5af0c
Faracoeng <alfmazucheli@gmail.com>
2019-09-23 08:21:45 -0300

    AE2 - Atividade extra classe 2 add (não compilando)

---
+++ AE2/incrementadorGray.vhd
@@ -0,0 +1,39 @@
+LIBRARY IEEE;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity incrementadorGray is
+        generic (N: natural := 4);
+        port(
+        gray :in std_logic_vector(N-1 downto 0);
+        y  : out std_logic_vector(N-1 downto 0)
+
+        );
+
+end entity;
+
+architecture andre01 of incrementadorGray is
+
+        signal bin_i: std_logic_vector(gray'range);
+
+begin
+-- conversao de gray para binario
+loopForGenerate01:
+        for i in bin_i'left-1 downto 0 generate
+                bin_i(i) <= gray(i) xor bin_i(i+1);
+        end generate;
+
+bin_i(N-1) <= gray(N-1);
+
+-- incrementar binario
+
+bin_i <= std_logic_vector(unsigned(bin_i) + '1');
+
+-- conversor de binario para gray
+loopForGenerate02:
+        for i in bin_i'left-1 downto 0 generate
+                y(i) <= bin_i(i) xor bin_i(i+1);
+        end generate;
+y(N-1) <= bin_i(N-1);
+
+end architecture;
