9866f677fd7aa5c96cfc530a1b002e5845172a97
Faracoeng <alfmazucheli@gmail.com>
2019-10-11 19:35:12 -0300

    AE3 concluido

--- AE3/bin27seg.vhd
+++ AE3/bin27seg.vhd
@@ -1,80 +1,57 @@
 library IEEE;
-use ieee.std_logic_1164.all; 
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
 entity bin27seg is
 	-- generic(<name>	: <type>  :=	<default_value>);
-	port
-	(	-- Input ports
-		clk, rst	: in  std_logic;
+	port(	
 		x : in std_logic_vector(3 downto 0);
 		y	: out  std_logic_vector(6 downto 0)
 	);
 end bin27seg;
 
--- Library Clause(s) (optional)
--- Use Clause(s) (optional)
-
 architecture andre01 of bin27seg is
-	--fazer um hexadecimal
-	signal count_sig : std_logic_vector(16 downto 0);--integer range 0 to 9;
+	signal count_sig : integer range 0 to 15;--integer range 0 to 9;
 
 begin
-contar:
-	process (rst, clk) is
-	variable count : std_logic_vector(16 downto 0);--integer range 0 to 9;
-	begin
-	if (rst = '1') then
-		count := 0; --variavel
-	elsif (rising_edge(clk)) then	
-		if count = 9 then	
-		--if count > 8 then    -se quiser otimizar o circuito
-			count := 0;
-		else count := count + 1;
-		end if;
-	end if;
-	count_sig <= count;
-	end process;
+	count_sig <= to_integer(unsigned(x));
 	process (count_sig) is 
 		begin
 			case count_sig is
-				when "0000" => --0
-					-- Sequential Statement(s)
+				when 0 => --0
 					y <= "0000001";
-				when "0001" =>	--1
+				when 1 =>	--1
 					y <= "1001111";
-				when "0010" =>	--2
+				when 2 =>	--2
 					y <= "0010010";
-				when "0011" =>	--3
+				when 3 =>	--3
 					y <= "0000110";
-				when "0100" =>	--4
+				when 4 =>	--4
 					y <= "1001100";
-				when "0101" =>	--5
+				when 5 =>	--5
 					y <= "0100100";
-				when "0110" =>	--6
+				when 6 =>	--6
 					y <= "0100000";
-				when "0111" =>	--7
+				when 7 =>	--7
 					y <= "0001111";
-				when "1000" =>	--8
+				when 8 =>	--8
 					y <= "0100000";
-				when "1001" =>	--9
+				when 9 =>	--9
 					y <= "1101111";
-				when "1010" =>	--A
+				when 10 =>	--A
 					y <= "0001000";
-				when "1011" =>	--b
+				when 11 =>	--b
 					y <= "1100000";
-				when "1100" =>	--c
+				when 12 =>	--c
 					y <= "0110001";
-				when "1101" =>	--D
+				when 13 =>	--D
 					y <= "1000010";
-				when "1110" =>	--E
+				when 14 =>	--E
 					y <= "0110000";
-				when "11111" =>--F
+				when 15 => --F
 					y <= "0111000";
-
 				when others =>
-					-- Sequential Statement(s)
 					y <= "0000000";
 			end case;
-
 	end process;
 end architecture;
