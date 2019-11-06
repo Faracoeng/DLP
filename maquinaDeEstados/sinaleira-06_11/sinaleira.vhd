fb0cb595ca48e1eadfbb210ef2d767bb0d6f2c64
Faracoeng <alfmazucheli@gmail.com>
2019-11-06 09:18:16 -0300

    maquina de estados - sinaleira add editada

--- maquinaDeEstados/sinaleira-06_11/sinaleira.vhd
+++ maquinaDeEstados/sinaleira-06_11/sinaleira.vhd
@@ -20,7 +20,7 @@ BEGIN
 	PROCESS (clk, rst)
 	BEGIN
 		IF (rst = '1') THEN
-			pr_state <= ST0; -- Se a o rst for 1, vai para o ST0
+		--	pr_state <= ST0; -- Se a o rst for 1, vai para o ST0
 		ELSIF (clk'EVENT AND clk = '1') THEN
 			pr_state <= nx_state;
 		END IF;
@@ -29,96 +29,95 @@ BEGIN
  
 
 	------Logica Combinacional da FSM:------------
-	PROCESS (pr_state, c5, c10, c25)
-		BEGIN
-			P <= '0';
-			s5 <= '0'; -- Para nao gerar latch
-			s10 <= '0';
+	
+--	PROCESS (pr_state, c5, c10, c25)
+--		BEGIN
+--			P <= '0';
+--			s5 <= '0'; -- Para nao gerar latch
+--			s10 <= '0';
 
-			CASE pr_state IS
+		--	CASE pr_state IS
  
-				WHEN ST0 => 
+				--WHEN ST0 => 
+ --ANDRE :D
+
+		-- LOGICA DA SINALEIRA AQUI
  
+--					IF (c5 = '1') THEN
+--						nx_state <= st5;
+--					ELSIF (c10 = '1') THEN
+--						nx_state <= st10;
+--					ELSIF (c25 = '1') THEN
+--						nx_state <= st25;
+--					ELSE
+--						nx_state <= ST0;
+--					END IF;
+--				WHEN ST5 => 
+--					IF (c5 = '1') THEN
+--						nx_state <= st10;
+--					ELSIF (c10 = '1') THEN
+--						nx_state <= st15;
+--					ELSIF (c25 = '1') THEN
+--						nx_state <= st30;
+--					ELSE
+--						nx_state <= ST5;
+--					END IF;
+--
+--				WHEN ST10 => 
+--					IF (c5 = '1') THEN
+--						nx_state <= st15;
+--					ELSIF (c10 = '1') THEN
+--						nx_state <= st20;
+--					ELSIF (c25 = '1') THEN
+--						nx_state <= st35;
+--					ELSE
+--						nx_state <= ST10;
+--					END IF;
+--					
+--				WHEN ST15 => 
+--					IF (c5 = '1') THEN
+--						nx_state <= st20;
+--					ELSIF (c10 = '1') THEN
+--						nx_state <= st25;
+--					ELSIF (c25 = '1') THEN
+--						nx_state <= st40;
+--					ELSE
+--						nx_state <= ST15;
+--					END IF;
+--
+--				WHEN ST20 => 
+--
+--					IF (c5 = '1') THEN
+--						nx_state <= st25;
+--					ELSIF (c10 = '1') THEN
+--						nx_state <= st30;
+--					ELSIF (c25 = '1') THEN
+--						nx_state <= st45;
+--					ELSE
+--						nx_state <= ST20;
+--					END IF;
+--					
+--				WHEN ST25 => 
+--					p <= '1';
+--					nx_state <= ST0;
+--
+--				WHEN ST30 => 
+--					s5 <= '1';
+--					nx_state <= ST25;
+--
+--				WHEN ST35 => 
+--					s10 <= '1';
+--					nx_state <= ST25;
+--					p <= '1';
+--				WHEN ST40 => 
+--					s5 <= '1';
+--					s10 <= '1';
+--					nx_state <= ST30;
+--
+--				WHEN ST45 => 
+--					s10 <= '1';
+--					nx_state <= ST35;
 
-					--Se entrar 5 centavos, estado 5 (st5)
-					--Se entrar 10 centavos, estado 10 (st10)
-					--Se entrar 25 centavos, estado 25 (st25)
-					--Senao mantem estado 0
-					IF (c5 = '1') THEN
-						nx_state <= st5;
-					ELSIF (c10 = '1') THEN
-						nx_state <= st10;
-					ELSIF (c25 = '1') THEN
-						nx_state <= st25;
-					ELSE
-						nx_state <= ST0;
-					END IF;
-				WHEN ST5 => 
-					IF (c5 = '1') THEN
-						nx_state <= st10;
-					ELSIF (c10 = '1') THEN
-						nx_state <= st15;
-					ELSIF (c25 = '1') THEN
-						nx_state <= st30;
-					ELSE
-						nx_state <= ST5;
-					END IF;
-
-				WHEN ST10 => 
-					IF (c5 = '1') THEN
-						nx_state <= st15;
-					ELSIF (c10 = '1') THEN
-						nx_state <= st20;
-					ELSIF (c25 = '1') THEN
-						nx_state <= st35;
-					ELSE
-						nx_state <= ST10;
-					END IF;
-				WHEN ST15 => 
-					IF (c5 = '1') THEN
-						nx_state <= st20;
-					ELSIF (c10 = '1') THEN
-						nx_state <= st25;
-					ELSIF (c25 = '1') THEN
-						nx_state <= st40;
-					ELSE
-						nx_state <= ST15;
-					END IF;
-
-				WHEN ST20 => 
-
-					IF (c5 = '1') THEN
-						nx_state <= st25;
-					ELSIF (c10 = '1') THEN
-						nx_state <= st30;
-					ELSIF (c25 = '1') THEN
-						nx_state <= st45;
-					ELSE
-						nx_state <= ST20;
-					END IF;
-
-				WHEN ST25 => 
-					p <= '1';
-					nx_state <= ST0;
-
-				WHEN ST30 => 
-					s5 <= '1';
-					nx_state <= ST25;
-
-				WHEN ST35 => 
-					s10 <= '1';
-					nx_state <= ST25;
-					p <= '1';
-				WHEN ST40 => 
-					s5 <= '1';
-					s10 <= '1';
-					nx_state <= ST30;
-
-				WHEN ST45 => 
-					s10 <= '1';
-					nx_state <= ST35;
-
-			END CASE;
-			------Seção de Saída (opcional):-------
-			-- PROCESS (clk, rst)
-			-- BEGIN
+			--END CASE;
+		--END PROCESS;
+	END ARCHITECTURE;
